ecbd99a0265246c4b829c263b2aaee246d5dfa9a
Dmitriy <patrickjsmirnov@gmail.com>
2020-10-20 13:26:44 +0300

    change postgres password

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 const env = process.env.NODE_ENV || 'development';
 
 const host = env === 'development' ? 'localhost' : '81.163.28.64'
-const password = env === 'development' ? '123' : 'postgres'
+const password = env === 'development' ? '123' : 'root'
 
 exports.pool = new Pool({
   user: 'postgres',
